bf1c9374b9062df5ff4d93106a30fc09b8461b24
Tállyto Rodrigues <rodrigues.tallyto@hotmail.com>
2020-10-24 20:27:38 -0400

    ensure getCurrency throw if repository trhow

---
+++ src/currencies/currencies.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+
+export class CurrenciesRepository {
+  async getCurrency(currency: string): Promise<any> {
+  }
+}
+
+@Injectable()
+
+export class CurrenciesService {
+  constructor(private currenciesRepository: CurrenciesRepository){
+
+  }
+  async getCurrency(currency: string): Promise<any> {
+    try {
+      await this.currenciesRepository.getCurrency(currency)
+    } catch (error) {
+      throw new InternalServerErrorException()
+    }
+  }
+  
+
+
+ 
+}
