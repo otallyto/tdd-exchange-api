51b959e49347b9f5e2a052978fc785cc2e41270f
Tállyto Rodrigues <rodrigues.tallyto@hotmail.com>
2020-10-24 20:38:16 -0400

    ensuse called repository with correct params

--- src/currencies/currencies.service.spec.ts
+++ src/currencies/currencies.service.spec.ts
@@ -9,8 +9,8 @@ describe('CurrenciesService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CurrenciesService, 
-       {provide: CurrenciesRepository, useFactory: () => ({getCurrency: jest.fn() })}],
+      providers: [CurrenciesService,
+        { provide: CurrenciesRepository, useFactory: () => ({ getCurrency: jest.fn() }) }],
     }).compile();
 
     service = module.get<CurrenciesService>(CurrenciesService);
@@ -25,12 +25,22 @@ describe('CurrenciesService', () => {
   describe('getCurrency()', () => {
     it('should be throw if repository throw', async () => {
       (repository.getCurrency as jest.Mock).mockRejectedValue(new InternalServerErrorException())
-     await expect(service.getCurrency('INVALID')).rejects.toThrow(new InternalServerErrorException());
+      await expect(service.getCurrency('INVALID')).rejects.toThrow(new InternalServerErrorException());
     });
-    
+
     it('should be not throw if repository returns', async () => {
       await expect(service.getCurrency('USD')).resolves.not.toThrow()
-     });
+    });
+
+    it('should be called repository with correct params', async () => {
+      await expect(service.getCurrency('USD'))
+      expect(repository.getCurrency).toBeCalledWith('USD')
+    });
+
+    it('should be not throw if repository returns', async () => {
+      (repository.getCurrency as jest.Mock).mockReturnValue({ currency: 'USD', value: 1 })
+      expect(await service.getCurrency('USD')).toEqual({ currency: 'USD', value: 1 })
+    });
 
   });
 });
